773709447b5ffa80add5ff25b2e423a859c3be7c
Glenn Torrens <glenntorrens@gmail.com>
2025-05-29 18:17:53 +0900

    setting up auth with google and getting the backend set up

---
+++ app/appwrite/auth.ts
@@ -0,0 +1,42 @@
+export const loginWithGoogle = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const logoutUser = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const getUser = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const getGooglePicture = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const storeUserData = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const getExistingUser = async () => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
