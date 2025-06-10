5beb9b5d7dbf752e271b4e357e5d07b830b0ca64
Glenn Torrens <glenntorrens@gmail.com>
2025-06-10 16:30:35 +0900

    added form to create trips tsx but not working

--- app/routes/admin/create-trips.tsx
+++ app/routes/admin/create-trips.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { ComboBox, ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
 import { Header } from "~/components";
+import type { Route } from "./+types/create-trips";
 
-const createTrips = () => {
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+export const loader = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+  return data.map((country: any) => ({
+    name: country.flag + country.name.common,
+    coordinates : country.latlng,
+    value : country.name.common,
+    openStreetMap : country.maps?.openStreetMaps
+  }));
+}
+
+const createTrips = ({loaderData}: Route.ComponentProps) => {
+
+    const handleSubmit = () => {
+       const countries = loaderData as Country[]; 
+       console.log(countries);
+
+       const countryData = countries.map((country: any) => ({
+         text : country.name,
+         value : country.value
+       }))
     };
 
   return (
@@ -15,7 +34,12 @@ const createTrips = () => {
       />
       <section className="mt-2.5 wrapper-md">
         <form className="trip-form" onSubmit={handleSubmit}>
-
+          <div>
+            <label htmlFor="country">
+              Country
+            </label>
+            <ComboBoxComponent id="country" dataSource={countryData} fields={{text: 'text', value: 'value'} } placeholder="Select a country" className="combo-box"/>
+          </div>
         </form>
       </section>
     </main>
